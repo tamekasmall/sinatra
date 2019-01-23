78f0c69b67a80a7ebb236c425449d42e4c9e794b
Tameka <tamekasmall@hotmail.com>
2019-01-23 12:07:47 +0000

    Introducing params

--- app.rb
+++ app.rb
@@ -1,8 +1,11 @@
 require 'sinatra'
 
-get '/' do
-  'hello!'
-
+get '/random-cat' do
 @name = ["Amigo", "Oscar", "Viking"].sample
+erb(:index)
+end
+
+get '/named-cat' do
+@name = params[:name]
 erb(:index)
 end
